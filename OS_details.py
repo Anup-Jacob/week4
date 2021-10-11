a6b35482db8f97fdae9b2399697820f3249f3721
Anup Jacob <L00163455@student.lyit.ie>
2021-10-11 13:56:30 +0100

    First commit for week4

---
+++ OS_details.py
@@ -0,0 +1,37 @@
+# ---------------------------------
+
+# File          : OS_details.py
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  : 11/10/2021
+# Modified Date : 11/10/2021
+# Description   : Displaying the machine and Operating System details
+# Licensing     : Anup Jacob, LYIT
+# ----------------------------------
+import os
+import platform
+import sys
+# importing the standard modules of python to fetch the sys, os and related information from the machine
+
+
+def os_details():
+    # information regarding the Operating system and the machine details are displayed in the body of this function
+   print("\nThe OS and version of the system is : "+platform.system()+" "+platform.release())
+   print("The owner of the PC : " + os.environ["USERNAME"])
+   print("The name of the machine : " + os.environ["COMPUTERNAME"])
+   print("The processor in the machine : " + os.environ["PROCESSOR_ARCHITECTURE"])
+   print("The number of processors in the system : " + os.environ["NUMBER_OF_PROCESSORS"])
+   #print()
+
+if __name__ == '''__main__''':
+    os_details()
+    # os_details function is called to get the details of the Operating System
+    python_path = sys.exec_prefix
+    print("\nThe path in which the current file is present : "+python_path)
+    # infomation regarding the python file path is displayed here
+    python_version = sys.path[5]
+    print("The python version used in my system: "+python_version[17:len(python_version)])
+    # infomation regarding the python version is displayed here
+
+    #print(sys.path)
+    # print function to fetch the details based on the position during initial phase of programming

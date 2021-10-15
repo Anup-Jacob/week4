c528b630490633172eb9109c5685fa3ae9337e78
Anup Jacob <L00163455@student.lyit.ie>
2021-10-15 11:35:47 +0100

    Added Menu 1 contents to Option_Display.py

--- Option_Display.py
+++ Option_Display.py
@@ -8,34 +8,44 @@
 # Description   : Program to display menu and details.
 # Licensing     : Anup Jacob, LYIT
 # ----------------------------------
+from pprint import pprint
+
 
 def read_courses():
-    print("Tuple of courses")
+    print("Selected Choice: View List of courses")
+    print("The courses available in LYIT are: ")
+    print("1. MSc in Devops (DO)")
+    print("2. Msc in Cyber Security (CS)")
+    print("3. MSc in Cloud Computing (CC)")
+    courses = ('DO','CS','CC')
+    return courses
 
 def edit_courses():
-    print("List of courses")
+    print("Selected Choice: List of courses")
 
 def pretty_print():
-    print("Pretty Print data in the list")
+    print("Selected Choice: Pretty Print data in the list")
 
 def word_check():
-    print("Check for common words in the list")
+    print("Selected Choice: Check for common words in the list")
 
 
 if __name__ == '''__main__''':
 
-    a=0
+
     print("")
     print("Display Menu")
     print("------------")
     print("1. Display the list of courses in LYIT")
     a = input("Enter your Choice : ")
     if (a == '1'):
-        read_courses()
+        courses = read_courses()
+        print("Tuple of Courses available in LYIT is ")
+        print(courses)
     elif (a == '2'):
         edit_courses()
     elif (a == '3'):
-        pretty_print()
+        pprint(courses)
     elif (a == '4'):
         word_check()
     else:
